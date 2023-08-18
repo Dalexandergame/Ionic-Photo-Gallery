293db0c12df589462337c5590ac765b47fe76ab8
Ekunke Ebu <e.alex@pulse.digital>
2023-08-18 11:20:24 +0100

    completed-Photo-gallery-app-fixed-bugs-and-updated-ionic-and-capacitor-version

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,28 +2,50 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Photo Gallery</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
       
-      <ExploreContainer name="Tab 2 page" />
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script setup lang="ts">
 
-export default defineComponent({
+// import { defineComponent } from 'vue';
+import { IonIcon, IonPage, IonHeader, IonFab, IonFabButton, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
+import { camera, trash, close } from 'ionicons/icons';
+
+import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
+const { photos, takePhoto } = usePhotoGallery();
+
+/* export default defineComponent({
   name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
+  components: { IonIcon, IonHeader, IonFab, IonFabButton, IonToolbar, IonTitle, IonContent, IonPage, IonGrid, IonRow, IonCol, IonImg },
+  setUp() {
+    const { photos, takePhoto } = usePhotoGallery();
+
+    return { 
+      photos,
+      takePhoto,
+      camera,
+      trash,
+      close, 
+    };
+  }
+}); */
 </script>
